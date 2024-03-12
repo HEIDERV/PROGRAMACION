f897b70b52211fcfe532f9c44ee4b67c7d4ad34a
HEIDERV <162387950+HEIDERV@users.noreply.github.com>
2024-03-11 21:08:57 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+nombre = input("Digite su nombre: ")
+ingreso = int(input("Ingrese sus ventas mensuales: "))
+comisiones = int(ingreso * 0.13)
+
+print("Las comisiones de " + nombre + " son: " + str(comisiones))
+sueldo = int(ingreso + comisiones)
+print("El sueldo total de" + " "+ nombre + " " + "es:", sueldo)  
+print ("ejercicios de prueba")
+print ("pruebas con git")
