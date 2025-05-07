24240c805eccd2e492e0cbf435458af96b8126ff
Heider Villamarin <162387950+HEIDERV@users.noreply.github.com>
2025-05-06 20:47:09 -0500

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-nombre = input("Digite su nombre: ")
-ingreso = int(input("Ingrese sus ventas mensuales: "))
-comisiones = int(ingreso * 0.13)
-
-print("Las comisiones de " + nombre + " son: " + str(comisiones))
-sueldo = int(ingreso + comisiones)
-print("El sueldo total de" + " "+ nombre + " " + "es:", sueldo)  
-print ("ejercicios de prueba")
-print ("pruebas con git")
